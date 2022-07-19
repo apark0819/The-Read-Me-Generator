335a461be9e3ab080793727facdd8207cfb3181f
apark0819 <apark0819@gmail.com>
2022-07-18 23:11:14 -0700

    deleted extraneous lines

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,6 @@
 
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const util = require("util");
 const inquirer = require("inquirer");
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
